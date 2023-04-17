53b566dc7c94995b6a771d47e600d650bc8d8476
Kulpot <kulpotwahu@gmail.com>
2023-04-18 04:29:56 +0800

    Add project files.

---
+++ LoadEmbeddedResources/NewFolder1/ContentLoading.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoadEmbeddedResources.NewFolder1
+{
+    static class ContentLoading
+    {
+        public static string GetXmlContent()
+        {
+            Assembly assembly = Assembly.GetExecutingAssembly();
+            const string NAME = "LoadEmbeddedResources.NewFolder1.MyStuff.xml";
+
+            using(Stream stream = assembly.GetManifestResourceStream(NAME))
+            {
+                using(StreamReader reader = new StreamReader(stream))
+                {
+                    return reader.ReadToEnd();
+                }
+            }
+        }
+    }
+}
